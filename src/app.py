33ccd7a17d5cd3012eb9f97f74ab82d79d40d18f
Josué Valenzuela <168314449+JozueDev@users.noreply.github.com>
2025-06-20 20:01:04 +0000

    'test render'

--- src/app.py
+++ src/app.py
@@ -1,4 +1,28 @@
-from utils import db_connect
-engine = db_connect()
+from flask import Flask, request, render_template
+import pandas as pd
+import joblib
 
-# your code here
+app = Flask(__name__)
+
+modelo = joblib.load("modelo_clima.pkl")
+columnas = joblib.load("columnas_modelo.pkl")
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    resultado = None
+    if request.method == "POST":
+        datos = pd.DataFrame([{
+            "precipitation": float(request.form["precipitation"]),
+            "temp_max": float(request.form["temp_max"]),
+            "temp_min": float(request.form["temp_min"]),
+            "wind": float(request.form["wind"])
+        }])
+        resultado = modelo.predict(datos)[0]
+
+    return render_template("index.html", resultado=resultado)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
